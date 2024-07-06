a6ce2427ef4297adee2f0ddeeafbc4f6f3021490
nitsuh21 <nitsuhdemissew21@gmail.com>
2024-07-06 18:02:58 +0300

    driver detail

--- dash/urls.py
+++ dash/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('overview/', views.overview, name='overview'),
+    path('drivers/', views.drivers, name='drivers'),
+    path('orders/', views.orders, name='orders'),
+    path('driver-profile/<str:id>/', views.driver_profile, name='driver_profile'),
 ]
--- dash/views.py
+++ dash/views.py
@@ -6,3 +6,21 @@ def overview(request):
         print(request.POST)
     else:
         return render(request, 'dashboard/overview.html')
+    
+def orders(request):
+    if request.method == 'POST':
+        print(request.POST)
+    else:
+        return render(request, 'dashboard/orders.html')
+
+def drivers(request):
+    if request.method == 'POST':
+        print(request.POST)
+    else:
+        return render(request, 'dashboard/drivers.html')
+
+def driver_profile(request, id):
+    if request.method == 'POST':
+        print(request.POST)
+    else:
+        return render(request, 'dashboard/driver_profile.html')
